75d71dcfe94dd08722566ea37a4c8528e57bba74
Vlasta Vetra <vlastavetra@gmail.com>
2022-02-15 19:14:51 +0300

    redesign

--- project/src/components/module/Intro.jsx
+++ project/src/components/module/Intro.jsx
@@ -4,32 +4,21 @@ import styles from '../module/Intro.module.scss';
 function Intro() {
   return (
     <section className={styles.root}>
-      <div className={styles.imageContainer}>
-        <img
-          className={styles.image}
-          src="https://thumb.tildacdn.com/tild3663-3734-4734-a536-326363333561/-/format/webp/G_prisma.jpg"
-          alt="georgy reznikov"
-        />
-      </div>
-      <div className={styles.textContainer}>
+      <div className={styles.left}>
         <p className={styles.text}>
-          Hi, my name is George! <br /><br />
-          I&apos;m a designer and art-director from Moscow. <br />
-          I&apos;ve been working for famous agencies with the world&apos;s largest brands for&#160;8&#160;years. <br />
-          Now i&apos;m focused on creating useful interfaces and prefer long term meaningful projects.
+        Hi 🖖🏻
         </p>
-        <ul className={styles.text}>
-          <p>
-            My experience:
-          </p>
-          <li>— web and mobile apps</li>
-          <li>— landings</li>
-          <li>— packing design</li>
-          <li>— photo retouch</li>
-          <li>— graphic art</li>
-          <li>— motion design</li>
-        </ul>
+        <p className={styles.text}>
+        This is my portfolio. Here you can find my favorite works: apps, web sites and graphic design.
+        </p>
+        <p className={styles.text}>
+          I have a lot of skills in different design directions and 8 years of expirience, but now I’m focusing on UI design.
+        </p>
+        <p className={styles.textContact}>
+          You always can write me in Telegram or by e-mail
+        </p>
       </div>
+      <div className={styles.right}></div>
     </section>
   );
 }
--- project/src/components/pages/Main.jsx
+++ project/src/components/pages/Main.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import Header from '../module/Header';
-import Ticker from '../module/Ticker';
 import Intro from '../module/Intro';
-import Cardboard from '../module/Cardboard';
-import Gallery from '../module/Gallery';
-import SectionTitle from '../module/SectionTitle';
 import styles from './Main.module.scss';
 
 function Main() {
   return (
-    <div className={styles.root}>
+    <>
       <Header/>
       <main className={styles.main}>
-        <Ticker/>
         <Intro/>
-        <SectionTitle title='My favorite works'/>
-        <Cardboard/>
-        <SectionTitle title='And more'/>
-        <Gallery/>
       </main>
-    </div>
+    </>
   );
 }
 
