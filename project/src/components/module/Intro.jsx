5bd0c90994b6ca5dbdf6da6cbbb2855e2c9edc1c
Vlasta Vetra <vlastavetra@gmail.com>
2023-08-05 10:25:55 +0300

    main text changed

--- project/src/components/module/Intro.jsx
+++ project/src/components/module/Intro.jsx
@@ -1,35 +1,45 @@
-import React from 'react';
-import styles from '../module/Intro.module.scss';
+import React from "react";
+import styles from "../module/Intro.module.scss";
 
 function Intro() {
   return (
     <section className={styles.root}>
       <p className={styles.text}>
-        Hi 🖖🏻 <br/>
+        Hi 🖖🏻 <br />
         My name is George.
-        I&apos;m a multidisciplinary designer with 8 year experience.
-        Last years I specialize in UX/UI design for digital services and products.
-        Below you can find some of&nbsp;my&nbsp;works.
+        <br />
+        I'm a designer with 9 years of professional experience.
+        <br />
+        I love designing beautiful and intuitive interfaces on any platform.
+        <br />I constantly follow trends, learn fast and know how to makecomplex
+        things simple.
       </p>
       <p className={styles.textContact}>
         You always can write&nbsp;me in&nbsp;
-        <a className={styles.link}
-          href='https://t.me/georgeowlson'
-          rel='noopener noreferrer'
-          target='_blank'
+        <a
+          className={styles.link}
+          href="https://t.me/georgeowlson"
+          rel="noopener noreferrer"
+          target="_blank"
         >
           Telegram
         </a>
         <span> or&nbsp;by&nbsp;</span>
         <a
           className={styles.link}
-          href='mailto:georgereznikov86@gmail.com'
-          rel='noopener noreferrer'
+          href="mailto:georgereznikov86@gmail.com"
+          rel="noopener noreferrer"
         >
           email
         </a>
         <span> and download </span>
-        <a href="../../../CV_GeorgeReznikov.pdf" className={styles.link} download>my CV</a>
+        <a
+          href="../../../CV_GeorgeReznikov.pdf"
+          className={styles.link}
+          download
+        >
+          my CV
+        </a>
       </p>
     </section>
   );
